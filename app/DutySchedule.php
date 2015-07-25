7368b9197f587eeb473ec22739d6dd5d75e5fb45
Nathan Azaria <nathanajah@yahoo.com>
2015-07-25 21:38:02 +0800

    Added some models

--- app/DutySchedule.php
+++ app/DutySchedule.php
@@ -4,7 +4,7 @@ namespace blackbird;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class DutySchedule extends Model
 {
     public $timestamps = false;
 }
--- app/GrabbedDuty.php
+++ app/GrabbedDuty.php
@@ -4,7 +4,7 @@ namespace blackbird;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class GrabbedDuty extends Model
 {
     public $timestamps = false;
 }
